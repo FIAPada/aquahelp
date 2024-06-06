ac5431433de80da81bf170e2826cbe036b9819e5
Kou <mulukou@gmail.com>
2024-06-06 19:09:00 -0300

    verification now working on FE

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -5,7 +5,7 @@ export const getAxiosLoginInstance = (token: string | null) => {
     return axios.create({
       baseURL: "http://localhost:8000",
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `${token}`,
       },
     });
   else
